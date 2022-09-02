5544c1f4e2fa395dadbbaef9c322914fbc77d54d
DESKTOP-53M4E68\e4eoz <e4eozcelik@gmail.com>
2022-09-02 19:33:35 +0300

    scommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
+      <div>
+        <Route path='/Home' exact>
+          <Home/>
+        </Route> 
+        <Route path='/cart' exact>
+          <Cart/>
+        </Route> 
       </div>
+      </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import {Container,FormControl, Navbar } from 'react-bootstrap'
 
+import {
+    Badge,
+    Container,
+    Dropdown,
+    FormControl, 
+    Navbar,
+    Nav, 
+} from 'react-bootstrap'
+
+import {MdShoppingCart} from 'react-icons/md'
+import { Link } from 'react-router-dom';
 const Header = () => {
   return <Navbar bg='dark' variant = "dark" style={{ height: 80}}>
-    <Container>
-        
+    <Container>  
         <Navbar.Brand>
-            <a href='/'> Shopping cart</a>
+            <Link to='/'> Shopping cart</Link>
         </Navbar.Brand>
 
         <Navbar.Text className="search">
@@ -14,11 +23,22 @@ const Header = () => {
              style={{ width: 500 }} 
              placeholder='Search a product'
              className='m-auto'
-             
              />
-
-            
         </Navbar.Text>
+            
+        <Nav>
+        <Dropdown alignRight>
+        <Dropdown.Toggle  variant="success">
+             <MdShoppingCart color="white" fontSize="25px"   /> 
+            <Badge>{10}</Badge>
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu style={{ minWidth: 370}}>
+          <span style={{padding: 10}}>Cart is Empty!</span>
+        </Dropdown.Menu>
+      </Dropdown>
+     
+        </Nav>
 
     </Container>
   </Navbar>
